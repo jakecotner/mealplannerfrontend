7d9bd51b6d5ec0ef93615a8ef3d31078a256f058
jakecotner <jacobcotner1@gmail.com>
2024-12-08 12:51:03 -0800

    mealplannerfrontend 12/8/2024 ver1

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Inventory from "./inventory"; // Import Inventory from a separate file
+
+// Placeholder components for other sections
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function Recipes() {
+  return <h1>Recipes</h1>;
+}
+
+function MealPlans() {
+  return <h1>Meal Plans</h1>;
+}
+
+function GroceryLists() {
+  return <h1>Grocery Lists</h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/inventory">Inventory</Link></li>
+          <li><Link to="/recipes">Recipes</Link></li>
+          <li><Link to="/meal-plans">Meal Plans</Link></li>
+          <li><Link to="/grocery-lists">Grocery Lists</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/meal-plans" element={<MealPlans />} />
+        <Route path="/grocery-lists" element={<GroceryLists />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
